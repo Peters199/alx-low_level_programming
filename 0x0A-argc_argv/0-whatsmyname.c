26ae560222af425bbd6d3b78a4fc9625fdaf359c
Peters199 <obipeter491@gmail.com>
2023-04-09 21:59:22 +0000

    task1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
  * main - prints the name of the program
@@ -8,10 +8,10 @@
  *
  * Return: 0 always (success)
  */
-int main(int argc, chat * argv[])
+int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n" * argv[0]);
+	(void) argc;
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
