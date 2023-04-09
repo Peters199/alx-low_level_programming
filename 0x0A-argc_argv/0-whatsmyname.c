068716261b6c7856e0fdda865993781187a7fe89
Peters199 <obipeter491@gmail.com>
2023-04-09 17:03:41 +0000

    Argc and Argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 always (success)
+ */
+int main(int argc, chat * argv[])
+{
+	(void)argc;
+	printf("%s\n" * argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This readme file is about 0X0A-argc_argv
