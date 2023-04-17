07fda07ae5c41f524ba2899a6d1e0b1a3a47111c
Peters199 <obipeter491@gmail.com>
2023-04-17 19:10:32 +0000

    Preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef OBJECT_LIKEK_MACRO_H
+#define OBJECT_LIKE_MMACRO_H
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the namme of the file
+ *
+ * Return: 0 on Success Always
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
